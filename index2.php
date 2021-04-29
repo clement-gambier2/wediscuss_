77d6448cd8d204670d2cc7f09d6efbd998de4aaa
clément-gambier <clement.gambier@etu.umontpellier.fr>
2021-04-29 21:56:00 +0200

    CSS de deposer article et mise a jour search bar, photo

--- index2.php
+++ index2.php
@@ -1,21 +1,22 @@
 <?php include('include/nav.php'); ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-  <head>
+<head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="/css/home/home.css">
     <link rel="stylesheet" href="/css/home/enveloppe.css">
     <link rel="stylesheet" href="/css/home/search_bar.css">
+    <link rel="stylesheet" href="/css/hover.css">
     <title>Wediscus</title>
-  </head>
-  
-  <body>
-    
-    <div class="container-body">
+</head>
+
+<body>
+
+<div class="container-body">
 
     <!-- Enveloppe -->
 
-        <div id = "topBox" class="box">
+    <div id = "topBox" class="box">
         <a href="/deposer_article/form.html">
             <div class="letter-image">
                 <div class="animated-mail">
@@ -24,41 +25,43 @@
                         <div class="letter-border"></div>
                         <div class="letter-title"> Déposer une info</div>
 
-                            <div class="letter-stamp">
-                                <div class="letter-stamp-inner"></div>
-                            </div>
+                        <div class="letter-stamp">
+                            <div class="letter-stamp-inner"></div>
+                        </div>
                     </div>
                     <div class="top-fold"></div>
                     <div class="body"></div>
                     <div class="left-fold"></div>
                 </div>
                 <div class="shadow"></div>
-            </div> 
-        </a> 
-    
-        </div>
-  
-	  
-  <!-- Voir les infos  -->
-        <div class="box">
-                <div class="button ">
-                    <a href="articles/afficher_articles.php" class="hvr-bounce-in square_btn ">Voir les articles</a>
-                </div>
-        </div>
-
-                    <!-- Search bar -->
-        <div class="box">
-    <form name="form1" method="POST" action="../search.php">
-            <input type="text" placeholder = "Search" name="search" aria-label = "Search" required class="searchTerm">
-            <input type="submit" value = "Chercher" name ="submit" class="searchButton">
-    </form>
-
-        </div>
+            </div>
+        </a>
 
     </div>
 
 
+    <!-- Voir les infos  -->
+    <div class="box">
+        <div class="button ">
+            <a href="articles/afficher_articles.php" class="hvr-bounce-in square_btn ">Voir les articles</a>
+        </div>
+    </div>
 
-  </body>
-  <?php include('include/footer.php'); ?>
+    <!-- Search bar -->
+
+    <div class="box">
+        <form name="form1" method="POST" action="search.php">
+            <input type="text" placeholder = "Chercher" name="search" aria-label = "Search" required class="searchTerm">
+            <input type="submit" value = "Chercher" name ="submit" class="searchButton">
+        </form>
+        <img src="images/ressources/rechercher.svg" alt="girl searching" class="hvr-grow">
+    </div>
+
+</div>
+
+
+
+</body>
+<?php include('include/footer.php'); ?>
+
 </html>
